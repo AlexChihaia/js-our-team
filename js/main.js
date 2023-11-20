0a2c06190bd7ae435031c3a394eff2745fe0cd9a
AlexChihaia <alexchihaia84@gmail.com>
2023-11-20 15:12:56 +0100

    feat : milestone 0

---
+++ js/main.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const ourTeam = [
+    {
+        fullName: 'Wayne Barnett',
+        job: 'Founder & CEO',
+        profileImage: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        fullName: 'Angela Caroll',
+        job: 'Chief Editor',
+        profileImage: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        fullName: 'Walter Gordon',
+        job: 'Office Manager',
+        profileImage: 'walter-gordon-office-manager.jpg',
+    },
+    {
+        fullName: 'Angela Lopez',
+        job: 'Social Media Manager',
+        profileImage: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        fullName: 'Scott Estrada',
+        job: 'Developer',
+        profileImage: 'scott-estrada-developer.jpg',
+    },
+    {
+        fullName: 'Barbara Ramos',
+        job: 'Graphic Designer',
+        profileImage: 'barbara-ramos-graphic-designer.jpg',
+    }
+];
